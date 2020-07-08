6ac90e116a83e9c06e326c23487f3e9608013fe3
Shailesh Shringarpure - QIAGEN <ss_chaitra@yahoo.co.in>
2020-07-07 18:59:34 -0700

    completed reverse words in a string

--- src/hackerRank/reverseWordsInAString.java
+++ src/hackerRank/reverseWordsInAString.java
@@ -1,10 +1,12 @@
 package hackerRank;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class reverseWordsInAString {
 
-	String s = "this$ %is JA&VA";
+	String s = "thiss$ %is JA&VA";
+	String out;
 	public reverseWordsInAString() {
 		// TODO Auto-generated constructor stub
 	}
@@ -12,21 +14,38 @@ public class reverseWordsInAString {
 	public void reverse() {
 		String[] s1 = s.split(" "); //create an array of words based on space delimiter
 		ArrayList<String> al = new ArrayList<String>();
+		ArrayList<String> output = new ArrayList<String>();
 		for (int i = 0 ; i < s1.length; i++) { //create array list based out of string array.
 			al.add(s1[i]);
 		}
 		
-		for (String s2 : al ) {
+		for (String s2 : al ) { //for each word in a string
 			String tempResult = "";
 			
 			char[] a = s2.toCharArray() ; 
-			for (int j = 0; j < a.length; j++ ) {
+			for (int j = 0; j < a.length; j++ ) { //strip off non alpha characters
 				if ( Character.isLetter(a[j]) ) {
 					tempResult = tempResult + a[j];
 				}
+
 			}
+
+			char[] b = tempResult.toCharArray();
+
+			for (int l = 0, k = b.length-1; l < k; l++, k--) { //reverse one word at a time and store it in an array
+				char ch;
+				ch = b[k];
+				b[k] = b[l];
+				b[l] = ch;
+			}
+			output.add(Arrays.toString(b).substring(1,3*b.length-1).replaceAll(", ",""));
+
+
 		}
 		System.out.println(al);
+		System.out.println(output);
+
+
 	}
 
 	public static void main(String[] args) {
