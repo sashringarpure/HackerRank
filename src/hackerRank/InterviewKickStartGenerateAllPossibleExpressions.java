b13aa780083e36904a491e6517642be4d4753f08
Shailesh Shringarpure - QIAGEN <ss_chaitra@yahoo.co.in>
2020-12-30 14:59:22 -0800

    IK practise problems.

---
+++ src/hackerRank/InterviewKickStartGenerateAllPossibleExpressions.java
@@ -0,0 +1,91 @@
+package hackerRank;
+
+import jdk.jshell.JShell;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class InterviewKickStartGenerateAllPossibleExpressions {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,2,2};
+        String s = "6666666666";
+        String[] op = {"()","*"};
+        long target = 6;
+        String [] result;
+        ArrayList<String> res = new ArrayList<String>();
+
+//        for (int i = 0; i < op.length; i++ )
+//              recursion(arr, 0, op, "");
+        recursion(s, 0, op, "", target, res);
+        for (String o : res)
+            System.out.println(o);
+//        result = new String[res.size()];
+//        result = res.toArray(result);
+
+    }
+
+    public static void recursion(String s, int index, String[] op, String slate, long target, ArrayList<String> res) {
+
+        if ( index == (s.length()-1) ) {
+            slate = slate + s.substring(index); //evaluate the slate
+            slate = slate.replaceAll("\\(","").replaceAll("\\)","");
+            String origExpr = slate;
+            System.out.println("slate expression:" + slate);
+            slate = eval(slate,"*");
+            slate = eval(slate,"+");
+            if ( Long.parseLong(slate) == target )
+                res.add(origExpr);
+            System.out.println("slate value:" + slate);
+            System.out.println("slate List:" + res);
+        }
+        else {
+            for (int i = 0; i <= op.length-1; i++ ) {
+                recursion(s, index+1,op,slate + s.substring(index, index+1) + op[i], target, res);
+            }
+        }
+
+    }
+
+    public static String eval(String slate, String op) {
+
+        if ( slate.contains(op) ) {
+
+            int index = slate.indexOf(op);
+            if ( index > 0 ) {
+                int leftCounter = index - 1;
+                Long result = 0L;
+                while ( slate.substring(leftCounter-1 > 0 ? leftCounter-1 : leftCounter,index).chars().allMatch(Character::isDigit) && leftCounter >= 1 ) { leftCounter = leftCounter - 1;}
+                Long leftOperand = Long.parseLong(slate.substring(leftCounter,index));
+                int rightCounter = index+1;
+                while ( slate.substring(index+1,rightCounter).chars().allMatch(Character::isDigit) && rightCounter <= slate.length()-1 ) {rightCounter = rightCounter + 1;}
+                Long rightOperand = 0L;
+                if ( rightCounter > slate.length() - 1 )
+                    rightOperand = Long.parseLong(slate.substring(index+1));
+                else
+                    rightOperand = Long.parseLong(slate.substring(index+1,--rightCounter));
+                switch (op) {
+                    case "*":
+                        result = leftOperand*rightOperand;
+                        break;
+                    case "+":
+                        result = leftOperand+rightOperand;
+                        break;
+                }
+
+//                slate = slate.replace(slate.substring(Math.max(leftCounter,0),rightCounter),Integer.toString(result));
+                slate = slate.replaceFirst(Pattern.quote(slate.substring(leftCounter,rightCounter)),Long.toString(result));
+                System.out.println("slate recursion :" + slate);
+            }
+            return eval(slate,op);
+        } else {
+            return slate;
+        }
+
+    }
+
+
+}
--- src/hackerRank/MergeSort.java
+++ src/hackerRank/MergeSort.java
@@ -25,6 +25,23 @@ Left,mid,right
 11)3,3,4
 12)0,2,4
 
+Explanation:
+1. In the mersort function, make two recursive calls to this function.
+2. Before making a recursive call, split, the array into two (start+end/2). Make the first recursive call with end = mid
+and make the second recursive call with start = mid+1.
+3. The termination criteria for recursion would be that this function body should get executed only if start input param
+is less than the end input param. What this does is the recursive calls repeatedly split the input array into small sizes
+till we have one element from the LHS and the RHS of the array.
+4. In the merge function, we copy the elements into two new arrays, one for the LHS of the input array and the other
+for the RHS of the input array. We do this using a for loop till the length of the LHS and RHS of the array.
+5. We have a while loop to traverse the lengths of the LHS and RHS of the array, and in this loop, we compare the elements
+from the LHS and RHS arrays and substitute the smaller element in the main array. We increment the array index by one
+of the LHS or the RHS array after successful substitution. We also increment the array index of the main array after
+successfull substitutions.
+6. We, then have two while loops which run till the length of the LHS and RHS arrays for any residual elements. For e.g.
+the LHS array had more smaller elements than the RHS array, hence, the LHS array will get exhausted first by copying it's
+contents to the main array. Then, in one of the two residual arrays, the remaining elements of the LHS/RHS shall get
+copied to the main array.
  */
 
 import java.util.Arrays;
